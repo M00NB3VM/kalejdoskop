fd86e2407862442c6b9cd7dd89c071f446be33b0
Saga Vallin <saga.vallin@gmail.com>
2023-03-08 12:20:50 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+import { api } from "~/utils/api";
+
+const Home: NextPage = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>Kalejdoskop</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="" />
+      </Head>
+      <main>
+        <h1>Hello world</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
