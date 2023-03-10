376048da7fead13e36c35929787f188c997a350f
Saga Vallin <saga.vallin@gmail.com>
2023-03-10 14:32:29 +0100

    added styling to active room nav link

--- src/components/RoomNav.tsx
+++ src/components/RoomNav.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function RoomNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex">
-      <Link href="/roomone" className="m-4">
+      <Link
+        href="/roomone"
+        className="m-4"
+        style={{ color: router.pathname === "/roomone" ? "red" : "black" }}
+      >
         Rum 1
       </Link>
-      <Link href="/roomtwo" className="m-4">
+      <Link
+        href="/roomtwo"
+        className="m-4"
+        style={{ color: router.pathname === "/roomtwo" ? "red" : "black" }}
+      >
         Rum 2
       </Link>
-      <Link href="/roomthree" className="m-4">
+      <Link
+        href="/roomthree"
+        className="m-4"
+        style={{ color: router.pathname === "/roomthree" ? "red" : "black" }}
+      >
         Rum 3
       </Link>
     </nav>
