39ecca877d2f222826c7f62770931755f8ad9045
Saga Vallin <saga.vallin@gmail.com>
2023-03-23 14:12:38 +0100

    added list of cooperative partner organizations

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,11 +6,52 @@ import { motion } from "framer-motion";
 
 import ThemeToggleButton from "~/components/ThemeToggleButton";
 
+interface Partner {
+  name: string;
+  about: string;
+  url: string;
+}
+
 function About() {
+  const partners: Partner[] = [
+    {
+      name: "Mind • Livslinjen",
+      about:
+        "En anonym stödchatt där alla mellan 16 och 25 år kan prata med en vuxen om saker i livet som känns jobbiga. Stort som smått.",
+      url: "https://mind.se/hitta-hjalp/livslinjen",
+    },
+    {
+      name: "Mind • Självmordslinjen",
+      about:
+        "Till för dig som har tankar på att ta ditt liv eller har en närstående med sådana tankar. Här kan du chatta anonymt med våra volontärer dygnet runt, årets alla dagar.",
+      url: "https://mind.se/chatt",
+    },
+    {
+      name: "Storasyster",
+      about:
+        "Stöd till dig som blivit utsatt för våldtäkt eller andra sexuella övergrepp.",
+      url: "https://storasyster.org",
+    },
+    {
+      name: "UMO",
+      about:
+        "För alla som är mellan 13 och 25 år. Här kan du få kunskap om kroppen, sex, relationer, psykisk hälsa, alkohol och droger, självkänsla och mycket annat.",
+      url: "https://www.umo.se",
+    },
+    {
+      name: "Unizon",
+      about:
+        "Riksförbund för över 130 idéburna kvinnojourer, tjejjourer och ungdomsjourer som arbetar för ett jämställt samhälle fritt från våld.",
+      url: "https://www.unizonjourer.se/ung",
+    },
+  ];
+
   return (
-    <motion.div initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 0.3 }}>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+    >
       <Head>
         <title>Om Kalejdoskop</title>
       </Head>
@@ -24,8 +65,35 @@ function About() {
           Tillbaka
         </Link>
 
-        <h1 className="w-4/5 mx-auto text-xl mt-10">Om detta projekt</h1>
-        <p className="w-4/5 mx-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur corrupti quos impedit a, repellendus quam nemo nesciunt sunt. Veritatis magni illo, unde delectus soluta quaerat tempora nemo molestias quidem sed.</p>
+        <h1 className="mx-auto mt-10 w-4/5 text-xl">Om detta projekt</h1>
+        <p className="mx-auto w-4/5">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+          corrupti quos impedit a, repellendus quam nemo nesciunt sunt.
+          Veritatis magni illo, unde delectus soluta quaerat tempora nemo
+          molestias quidem sed.
+        </p>
+
+        <h3 className="m-4 mx-auto mt-10 w-4/5 text-lg">Samarbetspartners</h3>
+        <ul className="m-4 mx-auto mb-8 flex w-4/5 flex-wrap">
+          {partners.map((partner) => {
+            return (
+              <li
+                className="mr-4 max-w-[290px] rounded p-6 shadow-lg"
+                key={partner.name}
+              >
+                <Link href={partner.url}>
+                  <h6
+                    className="mb-4 border-b-2 border-b-accent
+                   text-lg"
+                  >
+                    {partner.name}
+                  </h6>
+                  <p>{partner.about}</p>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </main>
     </motion.div>
   );
